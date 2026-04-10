3d3b8ac4002dc5b18b664aeb8e4734f6fd0b668d
SidimaD <Sidihassan17@gmail.com>
2026-04-10 19:24:26 +0100

    Update preview.js

--- preview.js
+++ preview.js
@@ -1,35 +1,30 @@
 (function () {
     var previewForm = document.getElementById('previewform');
-    var url = location.search.substring(1).replace(/\/\/github\.com/, '//raw.githubusercontent.com').replace(/\/blob\//, '/');
+    var query = location.search.substring(1); 
+    
+    // Convert GitHub URL to Raw URL
+    var rawUrl = query.replace(/\/\/github\.com/, '//raw.githubusercontent.com')
+                      .replace(/\/blob\//, '/');
 
-    var fetchProxy = function (url, options, i) {
-        var proxy = [
-            '', 
-            'https://api.codetabs.com/v1/proxy/?quest='
-        ];
-        return fetch(proxy[i] + url, options).then(function (res) {
-            if (!res.ok) throw new Error('Cannot load ' + url);
-            return res.text();
-        }).catch(function (error) {
-            if (i === proxy.length - 1) throw error;
-            return fetchProxy(url, options, i + 1);
-        });
-    };
-
-    var loadHTML = function (data) {
-        if (data) {
-            // This replaces scripts so they can be injected manually after the DOM is written
-            data = data.replace(/<head([^>]*)>/i, '<head$1><base href="' + url + '">')
-                       .replace(/<script(\s*src=["'][^"']*["'])?(\s*type=["'](text|application)\/javascript["'])?/gi, '<script type="text/htmlpreview"$1');
-            document.open();
-            document.write(data);
-            document.close();
-            // Note: The original replaceAssets() function would go here if you need CSS/JS support
-        }
-    };
-
-    if (url && url.indexOf(location.hostname) < 0) {
-        fetchProxy(url, null, 0).then(loadHTML).catch(console.error);
+    if (query) {
+        // Use the CodeTabs proxy to bypass CORS
+        fetch('https://api.codetabs.com/v1/proxy/?quest=' + rawUrl)
+            .then(res => {
+                if (!res.ok) throw new Error('Target file not found (404)');
+                return res.text();
+            })
+            .then(data => {
+                // Replace relative paths so images/css load
+                data = data.replace(/<head([^>]*)>/i, '<head$1><base href="' + rawUrl + '">');
+                
+                document.open();
+                document.write(data);
+                document.close();
+            })
+            .catch(err => {
+                previewForm.style.display = 'block';
+                previewForm.innerHTML = '<p style="color:red">Error: ' + err.message + '</p><a href="index.html">Go Back</a>';
+            });
     } else {
         previewForm.style.display = 'block';
     }
